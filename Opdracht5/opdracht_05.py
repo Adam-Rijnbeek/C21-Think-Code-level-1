e3ae93616d4ace6af702600c79db9cd881a22a18
Adam <adam.rijnbeek@student.rocva.nl>
2021-10-27 11:15:35 +0200

    commit opdracht 5 think code

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,11 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+
+# Prints out the name of a favorite game
+def favorite_game(name):
+    print("One of my favorite games is", name)
+
+    favorite_game("Super Mario Odyssey")
+    favorite_game("GTA V")
+    favorite_game("Fortnite")
