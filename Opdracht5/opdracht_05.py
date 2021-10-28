151f798b3d3cca274187a06be397a5ee362e5688
Adam <adam.rijnbeek@student.rocva.nl>
2021-10-28 10:10:16 +0200

    eindopdracht think code

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,11 +1,24 @@
-#maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
-#elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
 
-# Prints out the name of a favorite game
+name = input("Wat is jouw naam?")
+
+print("Welkom bij de eindopdracht")
+
+print ("We gaan nu beginnen met de eindopracht")
+
+print("Wat zijn jouw favoriete games?") 
+
 def favorite_game(name):
-    print("One of my favorite games is", name)
+    print("Mijn favoriete games zijn", name)
 
     favorite_game("Super Mario Odyssey")
     favorite_game("GTA V")
     favorite_game("Fortnite")
+
+print("Was de eindopdracht goed?")
+
+if conditie:
+    print(Ja)
+else:
+    print(nee)
+
+
